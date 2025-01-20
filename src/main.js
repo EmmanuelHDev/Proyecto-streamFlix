00bd82cf0b4f750fc0586bc0180d53671faa7adb
Emmanuel Herrera <emmanuelherrerab31@gmail.com>
2025-01-20 03:56:34 -0500

    Agregue la secction de tendecia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
+
+window.addEventListener('load', () => {
+    const splashScreen = document.getElementById('splashScreen');
+    const main = document.getElementById('main');
+  
+    // Oculta el splash después de 3 segundos
+    setTimeout(() => {
+      splashScreen.style.display = 'none';
+      main.style.display = 'block';
+      document.body.style.overflow = 'auto'; // Habilita el scroll
+    }, 0); // 3000ms = 3 segundos
+  });
+
+
+
+const btnTendecia = document.getElementById('btnTendencia')
+const btnSalitTendencia = document.getElementById('salirTendencia')
+btnTendecia.addEventListener('click', ()=>{
+    const sectionTendencia = document.getElementById('tendencia-busquedad')
+    const tendencia_todas = document.getElementById('tendencia_todas')
+    const tendencia_carrusel = document.getElementById('tendencia_carrusel')
+    const bucarPeli = document.getElementById('buscarPelicula')
+    const header = document.querySelector('header')
+    sectionTendencia.classList.add('inactive')
+    tendencia_todas.classList.toggle('inactive')
+    tendencia_carrusel.classList.add('inactive')
+    bucarPeli.classList.add('inactive')
+    header.classList.add('inactive')
+})
+btnSalitTendencia.addEventListener('click', () => {
+    const sectionTendencia = document.getElementById('tendencia-busquedad')
+    const tendencia_todas = document.getElementById('tendencia_todas')
+    const tendencia_carrusel = document.getElementById('tendencia_carrusel')
+    const bucarPeli = document.getElementById('buscarPelicula')
+    const header = document.querySelector('header')
+
+    sectionTendencia.classList.remove('inactive')
+    tendencia_todas.classList.add('inactive')
+    tendencia_carrusel.classList.remove('inactive')
+    bucarPeli.classList.remove('inactive')
+    header.classList.remove('inactive')
+})
 console.log('hola a todos' + API_KEY)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        FondoPrincipa: '#222'
+      },
+      keyframes: {
+        fadeInOut: {
+          '0%, 100%': { opacity: 0 },
+          '50%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeInOut: 'fadeInOut 2s ease-in-out infinite',
+      },
+      backgroundImage:{
+        menu : 'url(/public/asset/svg/menu.svg)',
+        home: 'url(/public/asset/svg/home.svg)',
+        buscar: 'url(/public/asset/svg/buscar.svg)',
+        fav: 'url(/public/asset/svg/fav.svg)',
+        cuenta: 'url(/public/asset/svg/cuenta.svg)',
+      }
+    },
   },
   plugins: [],
 }
